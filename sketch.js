65bee1c7692ada2d74a7c1a18b032e81efb91c5e
Mikael2609 <mikael.gustavo.silva@escola.pr.gov.br>
2024-09-04 23:00:50 +0000

    🐕

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
+function setup() {
+      createCanvas(400, 400);
+        background("green");
+        }
 
+        function draw() {
+          stroke("black");
+            fill("purple");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 20);
+                          }
+                          }
